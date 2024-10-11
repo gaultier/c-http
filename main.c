bd472d84248306c5bc038bebd0d97c6bceb4c02b
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-10-11 23:02:13 +0200

    [wip]

---
+++ main.c
@@ -0,0 +1,90 @@
+#define _POSIX_C_SOURCE 200809L
+#include <errno.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+static void handle_connection(int conn_fd) {
+  uint8_t buf[1024] = {0};
+  const int n_read = recv(conn_fd, buf, sizeof(buf), 0);
+  if (n_read == -1) {
+    fprintf(stderr, "failed to recvfrom(2): %s\n", strerror(errno));
+    exit(errno);
+  }
+
+  const int n_written = send(conn_fd, buf, sizeof(buf), 0);
+  if (n_written == -1) {
+    fprintf(stderr, "failed to sendto(2): %s\n", strerror(errno));
+    exit(errno);
+  }
+}
+
+int main() {
+  const uint16_t port = 12345;
+  struct sigaction sa = {.sa_flags = SA_NOCLDWAIT};
+  int err = 0;
+  if (-1 == (err = sigaction(SIGCHLD, &sa, NULL))) {
+    fprintf(stderr, "Failed to sigaction(2): err=%s\n", strerror(errno));
+    exit(errno);
+  }
+
+  const int sock_fd = socket(PF_INET, SOCK_STREAM, 0);
+  if (sock_fd == -1) {
+    fprintf(stderr, "Failed to socket(2): %s\n", strerror(errno));
+    exit(errno);
+  }
+
+  int val = 1;
+  if ((err = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &val,
+                        sizeof(val))) == -1) {
+    fprintf(stderr, "Failed to setsockopt(2): %s\n", strerror(errno));
+    exit(errno);
+  }
+//  if ((err = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEPORT, &val,
+//                        sizeof(val))) == -1) {
+//    fprintf(stderr, "Failed to setsockopt(2): %s\n", strerror(errno));
+//    exit(errno);
+//  }
+
+  const struct sockaddr_in addr = {
+      .sin_family = AF_INET,
+      .sin_port = htons(port),
+  };
+
+  if ((err = bind(sock_fd, (const struct sockaddr *)&addr, sizeof(addr))) ==
+      -1) {
+    fprintf(stderr, "Failed to bind(2): %s\n", strerror(errno));
+    exit(errno);
+  }
+
+  if ((err = listen(sock_fd, 16 * 1024)) == -1) {
+    fprintf(stderr, "Failed to listen(2): %s\n", strerror(errno));
+    exit(errno);
+  }
+
+  while (1) {
+    const int conn_fd = accept(sock_fd, NULL, 0);
+    if (conn_fd == -1) {
+      fprintf(stderr, "Failed to accept(2): %s\n", strerror(errno));
+      return errno;
+    }
+
+    const pid_t pid = fork();
+    if (pid == -1) {
+      fprintf(stderr, "Failed to fork(2): err=%s\n", strerror(errno));
+      exit(errno);
+    } else if (pid == 0) { // Child
+      handle_connection(conn_fd);
+      exit(0);
+    } else { // Parent
+      // Fds are duplicated by fork(2) and need to be
+      // closed by both parent & child
+      close(conn_fd);
+    }
+  }
+}
