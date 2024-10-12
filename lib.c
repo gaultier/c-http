0e8c8162dbbca7619b0333861a4c2865486b5258
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-10-12 23:24:14 +0200

    some routing

--- lib.c
+++ lib.c
@@ -165,7 +165,7 @@ typedef struct {
 } DynArrayHttpHeaders;
 
 typedef struct {
-  Slice path;
+  Slice path; // FIXME: Should be a parsed URL/URI.
   HttpMethod method;
   DynArrayHttpHeaders headers;
   int err;
@@ -653,9 +653,17 @@ static void handle_client(int socket, HttpRequestHandleFn handle) {
 static HttpResponse my_http_request_handler(HttpRequest req, Arena *arena) {
   ASSERT(0 == req.err);
 
-  HttpResponse res = {.status = 201}; // Dummy status code.
-  http_response_push_header_cstr(&res, "Content-Type", "text/html", arena);
+  HttpResponse res = {0};
   http_response_push_header_cstr(&res, "Connection", "close", arena);
+
+  if (HM_GET == req.method &&
+      (slice_eq(req.path, slice_make_from_cstr("/")) ||
+       slice_eq(req.path, slice_make_from_cstr("/index.html")))) {
+    res.status = 200; // Dummy status code.
+    http_response_push_header_cstr(&res, "Content-Type", "text/html", arena);
+  } else {
+    res.status = 404;
+  }
 
   return res;
 }
