3660bb06dc0ed2f362ce3d0938e31483a3cbafd8
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-10-12 22:49:07 +0200

    build script

--- build.sh
+++ build.sh
@@ -1,3 +1,36 @@
 #!/bin/sh
 set -ex
-clang -std=c11 -Wall -Wextra -g main.c -fsanitize=address -Wno-gnu-alignof-expression -o main.bin
+
+EXTRA_FLAGS=""
+
+error() {
+	printf "ERROR: %s\n" "$1"
+	exit 1
+}
+
+build() {
+case $1 in 
+  debug)
+    EXTRA_FLAGS="-O0"
+    ;;
+  asan)
+    EXTRA_FLAGS="-fsanitize=address "
+    ;;
+  release)
+    EXTRA_FLAGS="-O3"
+    ;;
+	*)
+		error "Build mode \"$1\" unsupported!"
+		;;
+esac
+
+clang -std=c11 -Wall -Wextra -g -Wno-gnu-alignof-expression main.c -o main.bin "$EXTRA_FLAGS"
+}
+
+if [ $# -eq 0 ]; then
+	build debug
+elif [ $# -eq 1 ]; then
+  build $1
+else
+	error "Too many arguments!"
+fi
