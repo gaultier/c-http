21a7d8143dd80d6a272868788a310acf6facfcc3
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-10-26 13:19:29 +0200

    limit sqlite max mem

--- install.sh
+++ install.sh
@@ -4,15 +4,6 @@ set -ex
 
 CC="${CC:-clang}"
 
-curl --retry 5 -sSL https://www.sqlite.org/2024/sqlite-amalgamation-3470000.zip -o sqlite-amalgamation-3470000.zip
-# TODO: checksum check.
-
-unzip -u -o sqlite-amalgamation-3470000.zip
-
-cp sqlite-amalgamation-3470000/sqlite3.c sqlite-amalgamation-3470000/sqlite3.h  .
-rm -r ./sqlite-amalgamation-3470000 ./sqlite-amalgamation-3470000.zip
-
-
 SQLITE_OPTIONS="$(cat sqlite_options.txt)"
 
 # shellcheck disable=SC2086
